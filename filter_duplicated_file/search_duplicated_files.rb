fb725770486d20ae67358ca1d41b1471e5e76f8a
FionaDong <fiona.zy.dong@gmail.com>
2014-12-30 13:46:52 +0800

    folder structure change

---
+++ filter_duplicated_file/search_duplicated_files.rb
@@ -0,0 +1,31 @@
+require 'fileutils'
+
+Dir.chdir ARGV[0]
+
+#get all the files and directories under current folder.
+file_dir = Dir['**/*']
+
+binding.pry
+
+filenames = []
+files = []
+current_path = Dir.getwd + '/'
+target_path = current_path + 'duplicated_files/'
+
+#make target folder where duplicated files go to.
+FileUtils.mkdir target_path unless File.directory?(target_path)
+
+#eliminated directories from file_dir and get pure filenames and file objects.
+file_dir.each do |file|
+  next if File.directory?(file)
+  files << file
+  filenames << File.basename(file)
+end
+
+#get duplicated filenames
+target_filenames = filenames.select{|file| filenames.count(file) > 1}.uniq
+
+#traverse all the files and move filename duplicated that checked.
+files.each do |file|
+  FileUtils.move(current_path + file.to_s, target_path) if target_filenames.include?(File.basename(file))
+end
